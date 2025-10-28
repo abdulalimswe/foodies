1fb382dd5ba2f12c69bb0c2d1cab6c7c6e5787a5
diusec <abdulalim.swe@gmail.com>
2025-10-28 14:01:13 +0600

    feat: View List Food & Delete Food

--- adminpanel/src/pages/ListFood/ListFood.jsx
+++ adminpanel/src/pages/ListFood/ListFood.jsx
@@ -1,8 +1,73 @@
-import React from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useEffect, useState } from "react";
+import './ListFood.css';
 
 const ListFood = () => {
+    const [list, setList] = useState([]);
+    const fetchList = async() => {
+        const response = await axios.get('http://localhost:8080/api/foods/viewFoods');
+        console.log(response.data);
+
+        if(response.status == 200){
+            setList(response.data);
+        }
+        else{
+            toast.error('Error while reading the foods');
+        }
+    }
+
+    const removeFood = async (foodId) => {
+        const response = await axios.delete('http://localhost:8080/api/foods/'+foodId);
+        await fetchList();
+
+        if(response.status === 204){
+            toast.success('food removed.');
+        }
+        else{
+            toast.error('error for food remove');
+        }
+    }
+
+    useEffect(() => {
+        fetchList();
+    }, []);
+
     return (
-        <div>ListFood</div>
+        <div className="py-5 row justify-content-center">
+            <div className="col-11 card">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Category</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            list.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>
+                                            <img src={item.imageUrl} alt="" height={48} width={48} />
+                                        </td>
+                                        <td>{item.name}</td>
+                                        <td>{item.category}</td>
+                                        <td>${item.price}.00</td>
+                                        <td className="text-danger">
+                                            <i className="bi bi-x-circle-fill" onClick={() => removeFood(item.id)}></i>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 
